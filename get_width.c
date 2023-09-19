e102e594172a5a84f0c13de66b64ad3cfda9eacc
Ketere <Keterel8@gmail.com>
2023-09-19 19:17:15 +0000

    corrections

--- get_width.c
+++ get_width.c
@@ -1,44 +1,32 @@
-#include <limits.h>
-#include <stdio.h>
 #include "main.h"
-
 /**
- * main - Entry point
+ * get_width - Calculates the width for printing
+ * @format: Formatted string in which to print the arguments.
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
  *
- * Return: Always 0
+ * Return: width.
  */
-int main(void)
+int get_width(const char *format, int *i, va_list list)
 {
-    int len;
-    int len2;
-    unsigned int ui;
-    void *addr;
-
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
-    ui = (unsigned int)INT_MAX + 1024;
-    addr = (void *)0x7ffe637541f0;
-    _printf("Length:[%d, %i]\n", len, len);
-    printf("Length:[%d, %i]\n", len2, len2);
-    _printf("Negative:[%d]\n", -762534);
-    printf("Negative:[%d]\n", -762534);
-    _printf("Unsigned:[%u]\n", ui);
-    printf("Unsigned:[%u]\n", ui);
-    _printf("Unsigned octal:[%o]\n", ui);
-    printf("Unsigned octal:[%o]\n", ui);
-    _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-    printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-    _printf("Character:[%c]\n", 'H');
-    printf("Character:[%c]\n", 'H');
-    _printf("String:[%s]\n", "I am a string !");
-    printf("String:[%s]\n", "I am a string !");
-    _printf("Address:[%p]\n", addr);
-    printf("Address:[%p]\n", addr);
-    len = _printf("Percent:[%%]\n");
-    len2 = printf("Percent:[%%]\n");
-    _printf("Len:[%d]\n", len);
-    printf("Len:[%d]\n", len2);
-    _printf("Unknown:[%r]\n");
-    printf("Unknown:[%r]\n");
-    return (0);
+int curr_i;
+int width = 0;
+for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+{
+if (is_digit(format[curr_i]))
+{
+width *= 10;
+width += format[curr_i] - '0';
+}
+else if (format[curr_i] == '*')
+{
+curr_i++;
+width = va_arg(list, int);
+break;
+}
+else
+break;
+}
+*i = curr_i - 1;
+return (width);
 }
